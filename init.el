f8759ea5ebaa178f2c79a03f4ee0f1cb942e3711
axtyax <darius.barbano@gmail.com>
2022-05-15 12:55:47 -0400

    initial

---
+++ init.el
@@ -0,0 +1,10 @@
+;; Load MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Remove GUI clutter
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+; 
